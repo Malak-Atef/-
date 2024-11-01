dfb4b8cff022babad165cb087c19e4c628d8ad0e
Malak Atef <79705774+Malak-Atef@users.noreply.github.com>
2024-11-01 19:24:51 +0200

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -20,7 +20,7 @@ messaging.onBackgroundMessage((payload) => {
     const notificationTitle = payload.notification.title;
     const notificationOptions = {
         body: payload.notification.body,
-        icon: 'logo.png'  // يمكنك تخصيص الأيقونة
+        icon: './icon-512x512.png'
     };
 
     self.registration.showNotification(notificationTitle, notificationOptions);
